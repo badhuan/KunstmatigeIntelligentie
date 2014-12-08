3dcb4d5762e2d6cd23f90986fa81a22736fa1517
Huan Nguyen <Huan500@hotmail.com>
2014-12-08 01:17:04 +0100

    Vertex has states

--- AI/SDLFramework/Vertex.h
+++ AI/SDLFramework/Vertex.h
@@ -16,8 +16,15 @@ public:
 
 
 	std::vector<Edge*>* getEdges();
+
+	enum StateType{ Wandering, Chase};
+	StateType getStateType(){ return stateType; };
+	void setStateType(StateType stateType){ this->stateType = stateType; };
+
 private:
 	int x, y;
 	std::vector<Edge*>* edges;
+
+	StateType stateType;
 };
 
